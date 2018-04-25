eca7266ca412cfb84c732d7bde358e91729ae4c7
Christopher YOVANOVITCH <yovano_c@outlook.com>
2018-04-25 14:56:23 +0200

    Add TagCode

--- tests/SwfReader/header.spec.ts
+++ tests/SwfReader/header.spec.ts
@@ -19,7 +19,7 @@ describe('SwfReader - Header', () => {
   });
 
   it('should read the signature', () => {
-    expect(header.signature).equals(Types.SwfCompression.Zlib);
+    expect(header.signature).equals(Types.SwfSignature.Zlib);
   });
 
   it('should read the fileLength', () => {
@@ -27,19 +27,19 @@ describe('SwfReader - Header', () => {
   });
 
   it('should read the frameSize', () => {
-    expect(header.frameSize.nBits).equals(42);
-    expect(header.frameSize.xMin).equals(42);
-    expect(header.frameSize.xMax).equals(42);
-    expect(header.frameSize.yMin).equals(42);
-    expect(header.frameSize.yMax).equals(42);
+    expect(header.frameSize.nBits).equals(16);
+    expect(header.frameSize.xMin).equals(0);
+    expect(header.frameSize.xMax).equals(25600);
+    expect(header.frameSize.yMin).equals(0);
+    expect(header.frameSize.yMax).equals(20480);
   });
 
   it('should read the frameRate', () => {
-    expect(header.frameRate).equals(40056);
+    expect(header.frameRate).equals(12800);
   });
 
   it('should read the frameCount', () => {
-    expect(header.frameCount).equals(48356);
+    expect(header.frameCount).equals(1);
   });
 
 });
