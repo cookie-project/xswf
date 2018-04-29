2d021025157cc9983aa09bd25f6524507a88e835
Christopher Yovanovitch <yovano_c@outlook.com>
2018-04-29 11:55:19 +0200

    Ordered Messages

--- tinker.ts
+++ tinker.ts
@@ -64,14 +64,19 @@ bodies.forEach((body) => {
 
 const messageClasses = doAbc.abcFile.instances.filter(c => c.name.kind === MultinameKind.QName && c.name.ns.name.includes('dofus.network.messages'))
 
-messageClasses.forEach((messageClass) => {
+const messages = messageClasses.map((messageClass) => {
   const name: IQName = messageClass.name as IQName;
   const protocolIdTrait = messageClass.class.traits.find((trait) => {
     return trait.kind === TraitKind.Const && trait.name.kind === MultinameKind.QName && trait.name.name === "protocolId";
   }) as ITraitSlot;
   const protocolId: number = protocolIdTrait.value.val as number;
-  console.log(protocolId, name.name);
+  return { id: protocolId, name: name.name };
 });
 
+const orderedMessages = messages.sort((a, b) => a.id - b.id);
+
+const toLog = orderedMessages.map((m) => `${m.id}: ${m.name}`);
+
+log(toLog);
 
 //log(body.code.map((instr) => InstructionCode[instr.code]));
